659b749cf5a4a4ade95cf42317999b413d5714c3
GreatOT <okemakindetobi@gmail.com>
2020-02-20 22:10:12 +0100

    fixed the menu

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    GameObject fader;
+
+    public void Awake()
+    {
+        fader = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    public void FaderAnimation()
+    {
+        fader.GetComponent<GameManager>().OnFadeComplete();
+    }
+}
